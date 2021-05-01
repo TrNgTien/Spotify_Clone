f19cd54c1bdf1497097312b0ee562065728e676d
kenpham4real <khanhphd.robert@gmail.com>
2021-05-01 22:58:18 +0700

    Create get method

---
+++ server/api/Songs/get.js
@@ -0,0 +1,22 @@
+const app = require('../../index');
+const sqlQuery = require('../my_sql_query');
+const dbConnection = require('../db_connection');
+
+// Get all songs    
+module.exports.getAllSongs = async (req, res) => {
+    try {
+        let connection = await dbConnection();
+        let getSongsQuery = "SELECT songname, uploadDate, numberOfLikes, duration FROM 'songs'"
+        let getSongs = await sqlQuery(connection, getSongsQuery);
+
+        connection.end();
+        return res.status(200).send(getSongs);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'error',
+            error: error
+        })
+    }
+
+}
