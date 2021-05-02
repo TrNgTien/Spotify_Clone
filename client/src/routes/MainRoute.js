a190f25a464ef5f5901db3229c5a413d773f8e2c
TrNgTien <ngoctien27122001@gmail.com>
2021-05-02 16:48:19 +0700

    Add the path to navigate to login-form, register-form and the main-page

--- client/src/routes/MainRoute.js
+++ client/src/routes/MainRoute.js
@@ -4,13 +4,16 @@
  */
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import LoginForm from "../pages/auth/LoginForm";
+import RegisterForm from "../pages/auth/RegisterForm";
 import MainPage from "../pages/MainPage";
-
 
 const MainRoute = () => {
   return (
     <Router>
-      <Route exact path = "/" component={ MainPage } />
+      <Route exact path="/main-page" component={MainPage} />
+      <Route exact path="/register-form" component={RegisterForm} />
+      <Route exact path="/" component={LoginForm} />
     </Router>
   );
 };
