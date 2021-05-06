2c840064de964c9deae023213e23145cc143bcfc
TrNgTien <ngoctien27122001@gmail.com>
2021-05-06 10:23:53 +0700

    Fix naming convention Delete folder hepler

--- server/controller/Users/UserQueries.js
+++ server/controller/Users/UserQueries.js
@@ -1,63 +1,57 @@
-const dbconnection = require('../../database/db_connection');
-const { autoGenerate } = require('../../helper/id');
-
-
+// const dbconnection = require('../../database/db_connection');
 const mysql = require("mysql");
-    // TODO: Global database config
-    const dbConfig = mysql.createConnection({
-        user: "b4bb330a3f009b",
-        host: "eu-cdbr-west-01.cleardb.com",
-        password: "4be56a74",
-        database: "heroku_1fcc54407741c0e",
-      });
+const ATTRIBUTE = require("./userAttribute");
+
+// TODO: Global database config
+const dbConfig = mysql.createConnection({
+  user: "b4bb330a3f009b",
+  host: "eu-cdbr-west-01.cleardb.com",
+  password: "4be56a74",
+  database: "heroku_1fcc54407741c0e",
+});
 
 module.exports = {
-    register: (data, callBack) => {
-        dbConfig.query(
-            `insert into users(username, password, userTypeID) 
-                        values(?,?,0)`,
-            [
-                data.username,
-                data.password
-            ],
-            (err, results, fields) => {
-                if(err){
-                    callBack(err);
-                }
-                else{
-                    return callBack(null, results);
-                }
-            }
-        )
-    },
-    
-    getUserByUserName: (username, callBack) => {
-        dbConfig.query(
-            `SELECT * FROM users WHERE username = ?`,
-            [username],
-            (error, results, fields) => {
-                if(error){
-                    callBack(error);
-                }
-                else{
-                    return callBack(null, results[0]);
-                }
-            }
-        )
-    },
+  register: (data, callBack) => {
+    dbConfig.query(
+      `INSERT INTO users(${ATTRIBUTE.userName}, ${ATTRIBUTE.password}, ${ATTRIBUTE.userTypeID}) 
+                        VALUES(?,?,0)`,
+      [data.userName, data.password],
+      (err, results, fields) => {
+        if (err) {
+          callBack(err);
+        } else {
+          return callBack(null, results);
+        }
+      }
+    );
+  },
 
-    getUsers: callBack => {
-        dbConfig.query(
-          `SELECT userID, userName, password, users.userTypeID, userstype.typeName 
+  getUserByUserName: (userName, callBack) => {
+    dbConfig.query(
+      `SELECT * FROM users WHERE ${ATTRIBUTE.userName} = ?`,
+      [userName],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        } else {
+          return callBack(null, results[0]);
+        }
+      }
+    );
+  },
+
+  getUsers: (callBack) => {
+    dbConfig.query(
+      `SELECT userID, ${ATTRIBUTE.userName}, ${ATTRIBUTE.password}, users.${ATTRIBUTE.userTypeID}, userstype.${ATTRIBUTE.typeName} 
            FROM users, userstype
-           WHERE users.userTypeID = userstype.userTypeID`,
-          [],
-          (error, results, fields) => {
-            if (error) {
-              callBack(error);
-            }
-            return callBack(null, results);
-          }
-        );
-      },
+           WHERE users.${ATTRIBUTE.userTypeID} = userstype.${ATTRIBUTE.userTypeID}`,
+      [],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results);
+      }
+    );
+  },
 };
