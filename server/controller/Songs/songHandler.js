a8e8f018c6c9f6f7e1a557a7fdef297f993ad048
kenpham4real <khanhphd.robert@gmail.com>
2021-05-08 23:31:16 +0700

    Handle song api request

--- server/controller/Songs/songHandler.js
+++ server/controller/Songs/songHandler.js
@@ -1,59 +1,128 @@
-const sqlQuery = require("../../database/my_sql_query");
-const dbConnection = require("../../database/db_connection");
+// const mysql = require("mysql");
+// const sqlQuery = require("../../database/my_sql_query");
+// const dbConnection = require("../../database/db_connection");
 
-import { SONG_ATTRIBUTE } from "./attribute";
+
+// const dbConfig = mysql.createConnection({
+//   user: "b4bb330a3f009b",
+//   host: "eu-cdbr-west-01.cleardb.com",
+//   password: "4be56a74",
+//   database: "heroku_1fcc54407741c0e",
+// });
 
 // Get all songs
-module.exports.getAllSongs = async (data, res) => {
-  try {
-    let connection = await dbConnection();
-    let getSongsQuery = `
-      SELECT 
-        ${SONG_ATTRIBUTE.songName}, 
-        ${SONG_ATTRIBUTE.uploadDate}, 
-        ${SONG_ATTRIBUTE.numberOfLikes}, 
-        ${SONG_ATTRIBUTE.duration} 
-      FROM songs`;
-    let getSongs = await sqlQuery(connection, getSongsQuery);
-
-    connection.end();
-    return res.status(200).send(getSongs);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      message: "error",
-      error: error,
+// module.exports.getSongs = async (req, res) => {
+//   try {
+//     let connection = await dbConnection();
+//     let getSongsQuery = `SELECT * FROM songs`;
+//     let result = await sqlQuery(connection, getSongsQuery);
+//     // let result = dbConfig.query(`SELECT songName, uploadDate, numberOfLikes, duration FROM songs`)
+//     connection.end();
+//     return res.status(200).json({ data: result, message: "OK!" });
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json({
+//       message: "error",
+//       error: error,
+//     });
+//   }
+// };
+const { getAllSongs, postSong } = require("./songQueries");
+const song_att = require("../Songs/attribute");
+function RequestHandler(err, results, res) {
+  if (err) {
+    console.log(err);
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  } else {
+    return res.status(200).json({
+      message: "Connection Successfully!",
+      data: results,
     });
   }
+}
+
+module.exports = {
+  getSongs: (req, res) => {
+    getAllSongs((err, results) => RequestHandler(err, results, res));
+    
+  },
+  postSongs: (req, res) => {
+    let body = req.body;
+    console.log("req body", body)
+    postSong(body, (err, results) => RequestHandler(err, results, res));
+  },
 };
 
-// Dealing when admin post song
-module.exports.postSong = async (data, res) => {
-  try {
-    let connection = await dbConnection();
-    let postSongsQuery = `
-      INSERT INTO songs(
-        ${SONG_ATTRIBUTE.songName}, 
-        ${SONG_ATTRIBUTE.uploadDate}, 
-        ${SONG_ATTRIBUTE.numberOfLikes}, 
-        ${SONG_ATTRIBUTE.duration}
-      )
-      VALUES(
-        ${data.songName},
-        ${data.uploadDate},
-        ${data.numberOfLikes},
-        ${data.duration}
-      )
-    `;
-    let postSong = await sqlQuery(connection, postSongsQuery);
+// // module.exports.getAllSongs = (req, res) => {
+// //   // Gửi lên server
+// //   const body = res.body;
 
-    connection.end();
-    return res.status(200).send(postSong);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      message: "error",
-      error: error,
-    });
-  }
-};
+// // }
+
+// // Dealing when admin post song
+// module.exports.postSong = async (data, res) => {
+//   try {
+//     let connection = await dbConfig();
+//     let postSongsQuery = `
+//       INSERT INTO songs(
+//         ${song_att.songName},
+//         ${song_att.uploadDate},
+//         ${song_att.numberOfLikes},
+//         ${song_att.duration}
+//       )
+//       VALUES(
+//         ${data.songName},
+//         ${data.uploadDate},
+//         ${data.numberOfLikes},
+//         ${data.duration}
+//       )
+//     `;
+
+//     let postSong = await sqlQuery(connection, postSongsQuery);
+
+//     connection.end();
+//     return res.status(200).send(postSong);
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json({
+//       message: "error",
+//       error: error,
+//     });
+//   }
+// };
+
+// class SongController {
+//   // [GET] /songs
+//   index(req, res) {
+//     // console.log('req', req)
+//     // res.render('songs')
+//     // let connection = await dbConfig();
+//     // let getSongsQuery = `SELECT songName, uploadDate, numberOfLikes, duration FROM songs`;
+//     // let result = dbConfig.query(getSongsQuery)
+//     // console.log('result', result)
+//     // if (result.OkPacket) return res.status(200).send(result);
+//   }
+
+//   // ${song_att.songName},
+//   // ${song_att.uploadDate},
+//   // ${song_att.numberOfLikes},
+//   // ${song_att.duration},
+//   // ${song_att.genre},
+//   // ${song_att.artist},
+//   // ${song_att.duration},
+//   // ${song_att.likeBy}
+
+//   // [GET] /songs/:yea
+
+//   show(req, res) {
+//     // res.send("Song details");
+//     let getSongsQuery = `SELECT songName, uploadDate, numberOfLikes, duration FROM songs`;
+//     let result = dbConfig.query(getSongsQuery)
+//     console.log('result', result)
+//     if (result.OkPacket) return res.status(200).send(result);
+//   }
+// }
+
+// module.exports = new SongController();
