af03d80082870e96367c34db0df9ba5f10e6af25
kenpham4real <khanhphd.robert@gmail.com>
2021-05-08 23:30:15 +0700

    Clean up global entry index file

--- server/index.js
+++ server/index.js
@@ -2,19 +2,27 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const PORT = process.env.PORT || 8080;
-// const songsRoutes = require("./routes/songsRoutes");
+const songsRoutes = require("./routes/songsRoutes");
 const userRouter = require("./routes/usersRoutes");
+const routes = require("./routes/index");
 
 app.use(cors());
 app.use(express.json());
 
 // Default port
-app.get("/", (req, res) => res.send({ error: true, message: "Hello" }));
-app.use("/userForm", userRouter);
+// app.get("/", (req, res) => res.send({ error: true, message: "Hello" }));
+// app.use("/userForm", userRouter);
+// app.use("/selectSong", songsRoutes);
+
+// app.get("/getMySong", (req, res) => {
+//   res.render("songs")
+// })
 
 // Listen to port
 app.listen(PORT, () => {
   console.log(` Server is running in ${PORT}`);
 });
+
+routes(app);
 
 module.exports = app;
