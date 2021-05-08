a73607b11cef49ffef70cb2ef1cc80425d9791ce
kenpham4real <khanhphd.robert@gmail.com>
2021-05-08 23:30:38 +0700

    Adjust global database config gile

--- server/database/db_connection.js
+++ server/database/db_connection.js
@@ -1,23 +1,29 @@
 const mysql = require("mysql");
 // TODO: Global database config
 const dbConfig = mysql.createConnection({
-    user: "b4bb330a3f009b",
-    host: "eu-cdbr-west-01.cleardb.com",
-    password: "4be56a74",
-    database: "heroku_1fcc54407741c0e",
-  });
-  
-  module.exports.db = async () =>
-    new Promise((resolve, reject) => {
-      const connection = mysql.createConnection(dbConfig);
-  
-      connection.connect((error) => {
-        if (error) {
-          reject(error);
-  
-          return;
-        }
-  
-        resolve(connection);
-      });
+  user: "b4bb330a3f009b",
+  host: "eu-cdbr-west-01.cleardb.com",
+  password: "4be56a74",
+  database: "heroku_1fcc54407741c0e",
+
+  // host: "127.0.0.1",
+  // user: "root",
+  // password: "thopham246",
+  // port: "3306",
+  // database: "heroku_test",
+});
+
+module.exports = async () =>
+  new Promise((resolve, reject) => {
+    const connection = mysql.createConnection(dbConfig);
+
+    connection.connect((error) => {
+      if (error) {
+        reject(error);
+
+        return;
+      }
+
+      resolve(connection);
     });
+  });
