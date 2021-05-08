d03180a88d0dfa13197a88633937e52f0f95bb75
TrNgTien <ngoctien27122001@gmail.com>
2021-05-08 16:16:33 +0700

    Config BE middleware

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8080;
 // const songsRoutes = require("./routes/songsRoutes");
 const userRouter = require("./routes/usersRoutes");
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // Default port
 app.get("/", (req, res) => res.send({ error: true, message: "Hello" }));
