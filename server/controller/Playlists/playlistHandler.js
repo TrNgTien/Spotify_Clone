5b972fa6fefe4688db6ab29395c204dba68cf50f
TrNgTien <ngoctien27122001@gmail.com>
2021-05-11 19:59:44 +0700

    Finish APIs for playlist

--- server/controller/Playlists/playlistHandler.js
+++ server/controller/Playlists/playlistHandler.js
@@ -1,54 +1,74 @@
-const {
-  postPlaylists,
-  getPlaylists,
-  deleteSongs,
-} = require("./playlistQueries");
+const db_connection = require("../../database/db_connection");
+const sqlQuery = require("../../database/my_sql_query");
 
-function getHandler(err, results, res) {
-  if (err) {
-    return res.status(500).json({
-      message: "Internal server error",
-    });
-  } else {
-    return res.status(200).json({
-      message: "Connection Successfully!",
-      playlists: { listSongs: results },
-    });
-  }
-}
-function postHandler(err, res) {
-  if (err) {
-    return res.status(500).json({
-      message: "Internal server error",
-    });
-  } else {
-    return res.status(200).json({
-      message: "Successfully!",
-    });
-  }
-}
+module.exports.postPlaylists = async (req, res) => {
+  try {
+    let playlistName = req.body.playlistName;
+    let userID = req.body.userID;
+    let connection = await db_connection();
 
-function deleteHandler(err, res) {
-  if (err) {
-    return res.status(500).json({
-      message: "Internal server error",
-    });
-  } else {
+    //-------------------Query get newest ID ----------------------
+    let query = `SELECT playlistID FROM playlists ORDER BY playlistID DESC LIMIT 1`;
+    let result = await sqlQuery(connection, query);
+    let newestID = result[0].playlistID;
+    let finalID = newestID + 1;
+    //--------------------------Query playlist newest ID------------------------
+
+    let queryInsert = `INSERT INTO playlists (playlistID, playlistName, userID) VALUES ( ${finalID}, ?, ${userID} )`;
+    let insertResults = await sqlQuery(connection, queryInsert, [playlistName]);
+    connection.end();
     return res.status(200).json({
-      message: "Deleted Successfully!",
+      message: "Insert Successfully!",
+    });
+  } catch (e) {
+    console.log(e);
+    return res.json({
+      e: e,
     });
   }
-}
-module.exports = {
-  getPlaylists: (req, res) => {
-    getPlaylists(null, (err, res) => getHandler(err, results, res));
-  },
-  postPlaylists: (req, res) => {
-    const body = req.body;
-    postPlaylists(body, (err, res) => postHandler(err, res));
-  },
-  deleteSongs: (req, res) => {
-    const body = req.body;
-    deleteSongs(body, (err, res) => deleteHandler(err, res));
-  },
+};
+module.exports.getPlaylists = async (req, res, next) => {
+  try {
+    let connection = await db_connection();
+    let query = `SELECT playlistName, songs.songID, songName, uploadDate, 
+          numberOfLikes, 
+          duration    
+          FROM  users, playlists, hasplaylists, songs
+          WHERE playlists.userID = users.userID
+          AND  hasplaylists.playlistID = playlists.playlistID
+          AND hasplaylists.songID = songs.songID`;
+    let result = await sqlQuery(connection, query);
+    connection.end();
+    if (result.length == 0) {
+      return res.json({
+        message: "No data avaiable!",
+      });
+    } else {
+      return res.send(result);
+    }
+  } catch (e) {
+    console.log(e);
+    return res.json({
+      e: e,
+    });
+  }
+};
+module.exports.deleteSongs = async (req, res) => {
+  let songDelete = req.body.songName;
+  try {
+    let connection = await db_connection();
+    let query = `DELETE FROM users, songs, playlists, hasPlaylist
+    WHERE songs.songID = hasPlaylist.songID
+    AND hasPlaylists.playlistID = playlists.playlistID
+    AND playlists.userID = users.userID
+    AND songName = ?`;
+    let result = await sqlQuery(connection, query, [songDelete]);
+    connection.end();
+    return res.send("Succesful!");
+  } catch (e) {
+    console.log(e);
+    return res.json({
+      e: e,
+    });
+  }
 };
