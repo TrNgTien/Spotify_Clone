36d9f66920e4e879a230c0bb03b3871db01c2228
TrNgTien <ngoctien27122001@gmail.com>
2021-05-14 16:53:43 +0700

    Create another to handle data

--- server/controller/Songs/songHandler.js
+++ server/controller/Songs/songHandler.js
@@ -1,6 +1,6 @@
 const sqlQuery = require("../../database/my_sql_query");
 const dbConnection = require("../../database/db_connection");
-// const SongAttribute = require("../../model/Song");
+const songFunction = require("./SongFunction");
 
 module.exports.getSongs = async (req, res) => {
   try {
@@ -38,88 +38,62 @@ module.exports.postSongs = async (req, res) => {
 
     //Insert the artistID, genreID for composeBy & belongsTo table
     //To know what song of which genre and whose compose the song
-    let artistIDQuery = `SELECT artistName FROM artists WHERE artistName = '${artistName}' `;
-    let genreIDQuery = `SELECT genreName FROM genres WHERE genreName = '${genreName}'`;
+    let artistNameQuery = `SELECT artistName FROM artists WHERE artistName = '${artistName}' `;
+    let genreNameQuery = `SELECT genreName FROM genres WHERE genreName = '${genreName}'`;
 
-    let getArtistIDQueryResult = await sqlQuery(connection, artistIDQuery);
-    let getGenreIDQueryResult = await sqlQuery(connection, genreIDQuery);
+    let getArtistNameResult = await sqlQuery(connection, artistNameQuery);
+    let getGenreNameResult = await sqlQuery(connection, genreNameQuery);
 
-    if (
-      getArtistIDQueryResult.length === 0 ||
-      getGenreIDQueryResult.length === 0
-    ) {
-      let getSongsID = `SELECT songID FROM songs ORDER BY songID DESC LIMIT 1`;
-      let getSongsIDQueryResult = await sqlQuery(connection, getSongsID);
-
-      let newestSongID = getSongsIDQueryResult[0].songID;
-      let insertArtistName = `INSERT INTO artists (artistName) VALUES (?)`;
-      let ArtistName = await sqlQuery(connection, insertArtistName, [
+    if (getArtistNameResult.length !== 0 && getGenreNameResult.length !== 0) {
+      songFunction.AllExisted(
         artistName,
-      ]);
-      let artistIDQuery = `SELECT artistID FROM artists WHERE artistName = '${artistName}' ORDER BY artistID DESC LIMIT 1`;
-      let genreIDQuery = `SELECT genreID FROM genres WHERE genreName = '${genreName}' ORDER BY genreID DESC LIMIT 1`;
-      let getArtistIDQueryResult = await sqlQuery(connection, artistIDQuery);
-      let getGenreIDQueryResult = await sqlQuery(connection, genreIDQuery);
-
-      let resultArtistID = getArtistIDQueryResult[0].artistID;
-      let resultGenreID = getGenreIDQueryResult[0].genreID;
-      // ------------------------------Insert Song---------------------------
-      let postSongInfoQuery = `INSERT INTO songs (songName, uploadDate, duration) VALUES (?, ?, ?)`;
-      let postSongQueryBasicResult = await sqlQuery(
+        songName,
+        uploadDate,
+        duration,
+        genreName,
         connection,
-        postSongInfoQuery,
-        [songName, uploadDate, duration]
+        sqlQuery,
+        res
       );
-
-      let postComposeByQuery = `INSERT INTO composeby (artistID, songID) VALUES (${resultArtistID}, ${newestSongID})`;
-      let postSongQueryComposeByResult = await sqlQuery(
+    } else if (
+      getArtistNameResult.length !== 0 &&
+      getGenreNameResult.length === 0
+    ) {
+      songFunction.ArtistExisted(
+        artistName,
+        songName,
+        uploadDate,
+        duration,
+        genreName,
         connection,
-        postComposeByQuery  
+        sqlQuery,
+        res
       );
-      let postBelongsToQuery = `INSERT INTO belongsto (genreID, songID) VALUES (${resultGenreID}, ${newestSongID})`;
-
-      let postSongQueryBelongsToResult = await sqlQuery(
+    } else if (
+      getArtistNameResult.length === 0 &&
+      getGenreNameResult.length !== 0
+    ) {
+      songFunction.GenreExisted(
+        artistName,
+        songName,
+        uploadDate,
+        duration,
+        genreName,
         connection,
-        postBelongsToQuery
+        sqlQuery,
+        res
       );
-      connection.end();
-      return res.status(200).json({
-        message: "Insert Successfully!",
-      });
     } else {
-      let getSongsID = `SELECT songID FROM songs ORDER BY songID DESC LIMIT 1`;
-      let getSongsIDQueryResult = await sqlQuery(connection, getSongsID);
-
-      let newestSongID = getSongsIDQueryResult[0].songID;
-      let artistIDQuery = `SELECT artistID FROM artists WHERE artistName = '${artistName}' ORDER BY artistID DESC LIMIT 1`;
-      let genreIDQuery = `SELECT genreID FROM genres WHERE genreName = '${genreName}' ORDER BY genreID DESC LIMIT 1`;
-      let getArtistIDQueryResult = await sqlQuery(connection, artistIDQuery);
-      let getGenreIDQueryResult = await sqlQuery(connection, genreIDQuery);
-      let resultArtistID = getArtistIDQueryResult[0].artistID;
-      let resultGenreID = getGenreIDQueryResult[0].genreID;
-      // ------------------------------Insert Song---------------------------
-      let postSongInfoQuery = `INSERT INTO songs (songName, uploadDate, duration) VALUES (?, ?, ?)`;
-      let postSongQueryBasicResult = await sqlQuery(
+      songFunction.RemainningCondition(
+        artistName,
+        songName,
+        uploadDate,
+        duration,
+        genreName,
         connection,
-        postSongInfoQuery,
-        [songName, uploadDate, duration]
+        sqlQuery,
+        res
       );
-      let postComposeByQuery = `INSERT INTO composeby (artistID, songID) VALUES (${resultArtistID}, ${newestSongID})`;
-    
-      let postSongQueryComposeByResult = await sqlQuery(
-        connection,
-        postComposeByQuery
-      );
-
-      let postBelongsToQuery = `INSERT INTO belongsto (genreID, songID) VALUES (${resultGenreID}, ${newestSongID})`;
-      let postSongQueryBelongsToResult = await sqlQuery(
-        connection,
-        postBelongsToQuery
-      );
-      connection.end();
-      return res.status(200).json({
-        message: "Insert Successfully!",
-      });
     }
   } catch (error) {
     console.log(error);
