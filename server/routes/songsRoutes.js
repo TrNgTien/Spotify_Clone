92892088ca58bd752e538b33a229ae792ca60cc7
TrNgTien <ngoctien27122001@gmail.com>
2021-05-14 17:32:34 +0700

    Fix naming variable

--- server/routes/songsRoutes.js
+++ server/routes/songsRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const SongHandler = require("../controller/Songs/SongHandler");
+const songHandler = require("../controller/Songs/songHandler");
 const ArtistHandler = require("../controller/Songs/ArtistHandler");
 const GenreHandler = require("../controller/Songs/GenreHandler");
 
-router.post("/postSongs", SongHandler.postSongs);
-router.get("/getAllSongs", SongHandler.getSongs);
+router.post("/postSongs", songHandler.postSongs);
+router.get("/getAllSongs", songHandler.getSongs);
 router.get("/getAllArtists", ArtistHandler.getArtists);
 router.get("/getAllGenres", GenreHandler.getGenres);
 
