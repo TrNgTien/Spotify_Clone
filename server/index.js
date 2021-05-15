e041256a5867713dac886e44d3e44f5ad4472173
TrNgTien <ngoctien27122001@gmail.com>
2021-05-15 12:25:44 +0700

    Try fix problem with cors

--- server/index.js
+++ server/index.js
@@ -4,17 +4,17 @@ const cors = require("cors");
 const PORT = process.env.PORT || 8080;
 const routes = require("./routes/index");
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json());   
 app.use(express.urlencoded({ extended: false }));
 routes(app);
 // Listen to port
